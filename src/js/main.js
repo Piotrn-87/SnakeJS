d2ae3ec60fcf0ab4804b5f81e2dcdee05fbaa6d0
Piotr Napieralski <piotrn-87@o2.pl>
2020-09-01 19:57:14 +0200

    Feat: Making a basic canvas

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,8 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
+const CANVAS = "canvas";
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+let ctx;
 
-// place your code below
-
-
-console.log(`Hello world!`);
-
-
+const canvas = document.getElementById(CANVAS);
+ctx = canvas.getContext("2d");
