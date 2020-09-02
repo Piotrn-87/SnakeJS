dee4db67f7effe39630eb31162dd6eefe02b21f5
Piotr Napieralski <piotrn-87@o2.pl>
2020-09-02 17:50:03 +0200

    Feat: Modified canvas

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,17 @@
 "use strict";
 
 const CANVAS = "canvas";
+const BOX = 32;
 
 let ctx;
 
 const canvas = document.getElementById(CANVAS);
 ctx = canvas.getContext("2d");
+
+const ground = new Image();
+ground.src = "../assets/img/ground.png";
+
+function draw() {
+  ctx.drawImage(ground, 0, 0);
+}
+let game = setInterval(draw, 100);
