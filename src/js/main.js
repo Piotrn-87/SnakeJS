5ad6f2039129d6bd60382952ee154b50dde19e1f
Piotr Napieralski <piotrn-87@o2.pl>
2020-09-05 11:10:19 +0200

    Feat: Set new version

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ const GROUNDIMG = new Image();
 const FOODIMG = new Image();
 
 let ctx;
+let snake = [];
 
 let food = {
   x: Math.floor(Math.random() * 17 + 1) * BOX,
@@ -21,6 +22,13 @@ FOODIMG.src = "../assets/img/food.png";
 
 function draw() {
   ctx.drawImage(GROUNDIMG, 0, 0);
+
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillStyle = i == 0 ? "green" : "white";
+    ctx.fillRect(snake[i].x, snake[i].y, 32, 32);
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(snake[i].x, snake[i].y, 32, 32);
+  }
 
   ctx.drawImage(FOODIMG, food.x, food.y);
 }
